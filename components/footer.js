42c5ed68fa8a63dbb0dc86ee3c43087fd94399cf
ssood12 <shivangi.sood0012@gmail.com>
2023-02-23 23:07:53 -0500

    Footer replaced

---
+++ components/footer.js
@@ -0,0 +1,107 @@
+import react from "react";
+import Image from "next/image";
+
+export default function Footer() 
+{
+ return(
+ <div>
+  <section className="py-10 bg-gray-50 overflow-hidden">
+        <div className="container mx-auto px-4">
+          <div className="py-16 px-8 mb-8 bg-white rounded-3xl">
+            <div className="max-w-7xl mx-auto">
+              <div className="flex flex-wrap items-center justify-center md:justify-between -m-4">
+                <div className="w-auto p-4">
+                  <a className="inline-block" href="#">
+                    <img
+                      src="assets/thinking-north-smart-cities-hackathon-logo.png"
+                      width="200"
+                      alt=""
+                    />
+                  </a>
+                </div>
+                <div className="w-auto p-4">
+                  <ul className="-m-6">
+                    <li className="inline-flex p-6">
+                      <a
+                        className="inline-block text-black hover:underline font-bold"
+                        href="https://analytics.umami.is/share/BKAQfqM8MxFHd9Z6/Seneca%20Hackathon"
+                        target="_blank"
+                      >
+                        Site Analytics
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className="w-auto p-4">
+                  <div className="flex flex-wrap -m-4">
+                    <div className="w-auto p-4">
+                      <a
+                        className="block"
+                        href="https://www.instagram.com/senecahackathon/"
+                        target="_blank"
+                      >
+                        <Image
+                          src="/zanrly-assets/images/insta.svg"
+                          width="25"
+                          height="25"
+                          alt=""
+                        />
+                      </a>
+                    </div>
+                    <div className="w-auto p-4">
+                      <a
+                        className="block"
+                        href="https://twitter.com/SenecaHackathon"
+                        target="_blank"
+                      >
+                        <Image
+                          width="25"
+                          height="25"
+                          src="/zanrly-assets/images/twitter.svg"
+                          alt=""
+                        />
+                      </a>
+                    </div>
+                    <div className="w-auto p-4">
+                      <a
+                        className="block"
+                        href="https://www.linkedin.com/company/seneca-hackathon/"
+                        target="_blank"
+                      >
+                        <Image
+                          width="25"
+                          height="25"
+                          src="/assets/linkedin.svg"
+                          alt=""
+                        />
+                      </a>
+                    </div>
+                    <div className="w-auto p-4">
+                      <a
+                        className="block"
+                        href="https://www.facebook.com/SenecaHackathon2022"
+                        target="_blank"
+                      >
+                        <Image
+                          width="25"
+                          height="25"
+                          src="/zanrly-assets/images/footers/fb.svg"
+                          alt=""
+                        />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <p className="text-center font-bold">
+            <span>Ⓒ Copyright. All rights reserved by </span>
+            <a className="text-red-600 hover:text-red-700" href="#">
+              Smart Cities Hackathon.
+            </a>
+          </p>
+        </div>
+      </section>
+ </div>)
+}
