4141991d01254eaf2c8ce4924615741950057a52
Santosharawn7 <santosharawn7@gmail.com>
2023-02-24 00:13:58 -0500

    FAQ updated

--- components/FAQAccordion.js
+++ components/FAQAccordion.js
@@ -1,31 +1,32 @@
 import React from "react";
 
 let FAQs = [
-  { question: "question 1", answer: "answer 1" },
-  { question: "question 2", answer: "answer 2" },
-  { question: "question 3", answer: "answer 3" },
-  { question: "question 4", answer: "answer 4" },
-  { question: "question 5", answer: "answer 5" },
-  { question: "question 6", answer: "answer 6" },
-  { question: "question 7", answer: "answer 7" },
-  { question: "question 8", answer: "answer 8" },
+  { question: "1. What is a Smart City?", answer: "A Smart City is an urban development vision to integrate information and communication technology (ICT) and Internet of Things (IoT) technology in a secure fashion to manage a city's assets and resources more efficiently for the city's future development" },
+  { question: "2. What are the criteria for a city to become a Smart City?", answer: "The criteria for a city to become a Smart City include a solid and reliable ICT infrastructure with efficient energy management systems, smart transportation, and mobility solutions. Moreover, with effective waste management systems and efficient public services, including the basic needs like electricity, water, heating, and other households that need to be managed within these areas." },
+  { question: "3. What is a Smart Cities hackathon?", answer: "A Smart Cities hackathon is where developers, designers, data scientists, and other tech-savvy individuals come together to develop innovative solutions for urban problems related to the development of Smart Cities." },
+  { question: "4. Who can participate in a Smart Cities hackathon?", answer: "Anyone with a passion for technology and innovation can participate in a Smart Cities hackathon, including students, developers, data scientists, and urban planners, if they think they have a solution to making an efficient and effective city transformation that is technically and realistically feasible." },
+  { question: "5. What is the format of a Smart Cities hackathon?", answer: "A Smart Cities hackathon typically begins with a series of presentations and workshops on the theme of Smart Cities. Participants then form teams and work on a solution to a specific problem related to the development of Smart Cities. The hackathon culminates in a pitch competition, where teams present their solutions to a panel of judges." },
+  { question: "6. What solutions are typically developed during a Smart Cities hackathon?", answer: "Solutions developed during a Smart Cities hackathon can range from smart transportation systems to efficient energy management systems, from waste management solutions to public safety systems, and from smart healthcare systems to citizen engagement platforms." },
+  { question: "7. How can I get involved in a Smart Cities hackathon?", answer: "You can get involved in a Smart Cities hackathon by searching for events in your area or by contacting organizations specializing in developing Smart Cities. You can also sign up for updates on hackathons and other events related to Smart Cities." },
 ];
 
 export default function FAQAccordion() {
   return (
+    
     <div
       id="accordion-flush"
       data-accordion="collapse"
-      data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white"
-      data-inactive-classes="text-gray-500 dark:text-gray-400"
+      data-active-classes="p-4 mb-2 bg-red-700 font-semibold text-bold rounded-xl dark:rounded dark:px-4 dark:bg-red-700 text-gray-200 dark:text-gray-200 text-2xl"
+      data-inactive-classes="p-4 mb-2 text-2xl text-bold text-red-700 dark:font-semibold dark:rounded dark:px-4 dark:text-red-700 bg-white"
     >
+      <h2 class="font-heading rounded-xl mb-6 text-3xl md:text-4xl lg:text-5xl text-red-700 font-black tracking-tight text-center">Frequently Asked Questions</h2>
       {FAQs.map((faq, index) => {
         return (
           <>
             <h2 id={"accordion-flush-heading-" + index}>
               <button
                 type="button"
-                class="flex items-center justify-between w-full py-5 font-medium text-left text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
+                class="flex px-4 text-2xl items-center justify-between rounded w-full py-5 text-left text-red-700 border-b-2 border-gray-600 dark:border-white-400 dark:text-white-400 dark:text-2xl"
                 data-accordion-target={"#accordion-flush-body-" + index}
                 aria-expanded="true"
                 aria-controls={"accordion-flush-body-" + index}
@@ -51,8 +52,8 @@ export default function FAQAccordion() {
               class="hidden"
               aria-labelledby={"accordion-flush-heading-" + index}
             >
-              <div class="py-5 font-light border-b border-gray-200 dark:border-gray-700">
-                <p class="mb-2 text-gray-500 dark:text-gray-400">
+              <div class="p-2 mb-2 rounded-xl text-xl font-light font-medium border-b-2 border-gray-500 dark:border-gray-600 text-justify">
+                <p class="mb-2 p-2 text-xl font-medium text-black dark:text-gray-500">
                   {faq.answer}
                 </p>
               </div>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import { useCallback, useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
-import Button from "../components/Button";
+import Foo from "../components/Button";
 import ClickCount from "../components/ClickCount";
 import styles from "../styles/home.module.css";
 
 import Image from "next/image";
+import Footer from "../components/footer";
 
 function throwError() {
   console.log(
@@ -1793,104 +1794,7 @@ function Home() {
 
       {/* // FOOTER SECTION */}
 
-      <section className="py-10 bg-gray-50 overflow-hidden">
-        <div className="container mx-auto px-4">
-          <div className="py-16 px-8 mb-8 bg-white rounded-3xl">
-            <div className="max-w-7xl mx-auto">
-              <div className="flex flex-wrap items-center justify-between -m-4">
-                <div className="w-auto p-4">
-                  <a className="inline-block" href="#">
-                    <img
-                      src="assets/thinking-north-smart-cities-hackathon-logo.png"
-                      width="200"
-                      alt=""
-                    />
-                  </a>
-                </div>
-                <div className="w-auto p-4">
-                  <ul className="-m-6">
-                    <li className="inline-flex p-6">
-                      <a
-                        className="inline-block text-black hover:underline font-bold"
-                        href="https://analytics.umami.is/share/BKAQfqM8MxFHd9Z6/Seneca%20Hackathon"
-                        target="_blank"
-                      >
-                        Site Analytics
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="w-auto p-4">
-                  <div className="flex flex-wrap -m-4">
-                    <div className="w-auto p-4">
-                      <a
-                        className="block"
-                        href="https://www.instagram.com/senecahackathon/"
-                        target="_blank"
-                      >
-                        <Image
-                          src="/zanrly-assets/images/insta.svg"
-                          width="25"
-                          height="25"
-                          alt=""
-                        />
-                      </a>
-                    </div>
-                    <div className="w-auto p-4">
-                      <a
-                        className="block"
-                        href="https://twitter.com/SenecaHackathon"
-                        target="_blank"
-                      >
-                        <Image
-                          width="25"
-                          height="25"
-                          src="/zanrly-assets/images/twitter.svg"
-                          alt=""
-                        />
-                      </a>
-                    </div>
-                    <div className="w-auto p-4">
-                      <a
-                        className="block"
-                        href="https://www.linkedin.com/company/seneca-hackathon/"
-                        target="_blank"
-                      >
-                        <Image
-                          width="25"
-                          height="25"
-                          src="/assets/linkedin.svg"
-                          alt=""
-                        />
-                      </a>
-                    </div>
-                    <div className="w-auto p-4">
-                      <a
-                        className="block"
-                        href="https://www.facebook.com/SenecaHackathon2022"
-                        target="_blank"
-                      >
-                        <Image
-                          width="25"
-                          height="25"
-                          src="/zanrly-assets/images/footers/fb.svg"
-                          alt=""
-                        />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <p className="text-center font-bold">
-            <span>Ⓒ Copyright. All rights reserved by </span>
-            <a className="text-red-600 hover:text-red-700" href="#">
-              Smart Cities Hackathon.
-            </a>
-          </p>
-        </div>
-      </section>
+      <Footer/>
 
       {/* FOOTER SECTION */}
     </>
